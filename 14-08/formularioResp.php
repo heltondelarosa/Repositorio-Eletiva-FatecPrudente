88b408cb9f07dfe18c7936688bfcd6c645313323
Helton <rosaheltonns@gmail.com>
2025-08-21 20:27:59 -0300

    Aula2108

--- 14-08/formularioResp.php
+++ 14-08/formularioResp.php
@@ -7,10 +7,14 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
 </head>
 <body>
-<?php
-    $nome = $_POST["nome"];
-    echo "Seja bem vindo(a) $nome";
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
-
+    <div class="container">
+        <?php
+            $n1 = $_POST["n2"];
+            $n2 = $_POST["n2"];
+            $soma = $n1 + $n2;
+            echo "<p>soma: $soma</p>"
+        ?>
+    </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </body>
 </html>
--- formularioSoma.php
+++ formularioSoma.php
@@ -19,6 +19,16 @@
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+
+<?php
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $n1 = $_POST["n2"];
+    $n2 = $_POST["n2"];
+    $soma = $n1 + $n2;
+    echo "<p>Soma: $soma</p>";
+  }
+?>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
